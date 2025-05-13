38f39a05e1b1cdb235613931d4ca85b479ee9af5
Aleksandra Fomina <sashavera1998@gmail.com>
2025-05-13 19:19:44 +0300

    modified:   cyphal_velocity_control_bldc/cyphal_velocity_control_bldc.ino

--- examples/other/cyphal_velocity_control_bldc/cyphal_velocity_control_bldc.ino
+++ examples/other/cyphal_velocity_control_bldc/cyphal_velocity_control_bldc.ino
@@ -219,8 +219,7 @@ void set_config(){
   SystemClock_Config();  // Настройка тактирования
   canfd = new CanFD();  // Создаем управляющий класс
   canfd->init(); // Инициализация CAN
-  canfd->write_default_params();  // Записываем дефолтные параметры для FDCAN (500000 nominal / 4000000 data)
-  hfdcan1 = canfd->get_hfdcan();  // Сохраняем конфиг
+  canfd->write_default_params();  // Записываем дефолтные параметры для FDCAN (1000000 nominal / 8000000 data)
   canfd->apply_config();  // Применяем их
   hfdcan1 = canfd->get_hfdcan();  // Сохраняем конфиг
   canfd->default_start();
