07b27953dfecfed74a3d7fdc778883f59eeadcdd
Nikolay Ivanov <57194638+NikolayIvanovWS@users.noreply.github.com>
2025-10-30 16:26:41 +0300

    Update can_send.py

--- examples/python/can_send.py
+++ examples/python/can_send.py
@@ -2,11 +2,9 @@ import can
 import struct
 import time
 
-
 can0 = can.interface.Bus(channel = 'can0', interface = 'socketcan', fd=True)
 msg = 2.5
 while(1):
-     t = time.time()
      data = bytearray(struct.pack("f", msg))
      msg_to_send = can.Message(arbitration_id=0x065, data = data)
      can0.send(msg_to_send)
