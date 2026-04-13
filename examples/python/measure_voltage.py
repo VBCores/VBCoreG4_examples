d330f215505f0f9fa7a860ca97442d2f835f8fd0
Aleksandra Fomina <sashavera1998@gmail.com>
2026-04-13 13:04:05 +0300

    example voltage measure added

---
+++ examples/python/measure_voltage.py
@@ -0,0 +1,25 @@
+# Чтение напряжения с canhat
+# Доустановите несколько библиотек, для этого в терминале введите:
+
+# pip3 install adafruit-circuitpython-ads1x15 --break-system-packages
+# sudo apt update
+# sudo apt install python3-lgpio -y 
+
+import time
+import board
+import busio
+
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# I2C
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# ADS1115
+ads = ADS.ADS1115(i2c, address=0x48)
+
+# канал AIN0 → это просто 0
+chan = AnalogIn(ads, 0)
+while True:
+    print(f"Voltage: {chan.voltage:.3f} V")
+    time.sleep(0.5)
