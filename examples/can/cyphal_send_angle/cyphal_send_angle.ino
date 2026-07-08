ef0e271ca20bfb604b941f5b25d78cd4279bcfbd
Aleksandra Fomina <sashavera1998@gmail.com>
2026-07-08 14:14:17 +0300

    modified:   can/cyphal_send_angle/cyphal_send_angle.ino

--- examples/can/cyphal_send_angle/cyphal_send_angle.ino
+++ examples/can/cyphal_send_angle/cyphal_send_angle.ino
@@ -1,92 +1,55 @@
-#include <utility>
-
 #include <VBCoreG4_arduino_system.h>
 #include <cyphal.h>
-#include <uavcan/si/sample/angle/Scalar_1_0.h>
+#include <cyphal_common_types.hpp>
 
-TYPE_ALIAS(AngleScalar, uavcan_si_sample_angle_Scalar_1_0)
+constexpr CanardNodeID NODE_ID = 2;
+constexpr CanardPortID ANGLE_TX_PORT_ID = 1000;
 
-static CanFD* canfd;
-static FDCAN_HandleTypeDef* hfdcan1;
-static std::shared_ptr<CyphalInterface> cyphal_interface;
+CanFD canfd;
+std::shared_ptr<ArduinoCyphal<>> cyphal;
 
-static bool _is_cyphal_on = false;
-static CanardNodeID NODE_ID;
+static CanardTransferID angle_transfer_id = 0;
 
-typedef uint32_t millis_t;
-typedef uint64_t micros_t;
+void send_angle(float radian)
+{
+    AngleSampleScalar msg{};
 
-void error_handler() {
-  Serial.println("error");
-  while (1) {}
+    msg.timestamp.microsecond = micros();
+    msg.radian = radian;
+
+    cyphal->send_msg(
+        &msg,
+        ANGLE_TX_PORT_ID,
+        &angle_transfer_id
+    );
 }
 
-UtilityConfig utilities(micros, error_handler);
+void setup()
+{
+    Serial.begin(115200);
 
-static constexpr CanardPortID ANGLE_PORT_ID = 1111;
-static constexpr micros_t MICROS_S = 1'000'000;
+    SystemClock_Config();
+    canfd.init();
+    canfd.write_default_params();
+    canfd.apply_config();
 
-void send_angle(float radian);
-void comms_loop(millis_t cur_millis);
-
-#define EACH_N(_value, _counter, N, code_blk) \
-  if (((_value) - (_counter)) >= (N)) {       \
-    code_blk                                  \
-    (_counter) = (_value);                    \
-  }
-
-void setup() {
-  Serial.begin(115200);
-
-  // Настройка FD CAN
-  SystemClock_Config();
-
-  canfd = new CanFD();
-  canfd->init();
-  canfd->write_default_params();   // 100000 nominal / 8000000 data
-  canfd->apply_config();
-  hfdcan1 = canfd->get_hfdcan();
-  canfd->default_start();
-
-  // Инициализация Cyphal
-  NODE_ID = 97;
-  size_t queue_len = 200;
-  cyphal_interface = CyphalInterface::create_heap<G4CAN, O1Allocator>(
-    NODE_ID, hfdcan1, queue_len, utilities
-  );
-
-  _is_cyphal_on = true;
+    cyphal = make_cyphal<ArduinoCyphal<>>(
+        canfd.get_hfdcan(),
+        NODE_ID,
+        "org.vbcores.angle_publisher"
+    );
+    cyphal->begin();
 }
 
-void loop() {
-  uint32_t t = micros();
-  millis_t current_millis = (millis_t)(t / 1000);
-  comms_loop(current_millis);
-}
+void loop()
+{
+    cyphal->cyphal_loop();
+    static uint32_t last_send = 0;
 
-void send_angle(float radian) {
-  if (!_is_cyphal_on) return;
-
-  AngleScalar::Type angle_msg = {};
-  static CanardTransferID angle_transfer_id = 0;
-
-  angle_msg.timestamp.microsecond = micros();
-  angle_msg.radian = radian;
-
-  cyphal_interface->send_msg<AngleScalar>(
-    &angle_msg,
-    ANGLE_PORT_ID,
-    &angle_transfer_id
-  );
-}
-
-static millis_t angle_send_time = 0;
-
-void comms_loop(millis_t cur_millis) {
-  cyphal_interface->loop();
-
-  EACH_N(cur_millis, angle_send_time, 1000, {
-    float some_angle = 1.0f;
-    send_angle(some_angle);
-  })
+    if (millis() - last_send >= 1000) // отправляем сообщение 1 раз в секунду
+    {
+        last_send = millis();
+        send_angle(1.0f); // угол равен 1 радиан
+        digitalToggle(LED2); // помигаем светодиодом
+    }
 }
